f03ccb8533bef675c2a101ab0bb79a6010e0d92d
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2017-11-13 16:03:14 +0100

    Premier jet

--- Trajets/Source.cpp
+++ Trajets/Source.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <cstring>
+#include <CString>
 using namespace std;
 
 int main() {
 	cout << "/////////////// POLYMORPHISME ///////////////";
 
 	string temp;
-	while (1) {
+	bool verif = false;
+
+	while (verif ==false) {
 		cout << "1. Ajouter trajet simple" << endl;
 		cout << "2. Ajouter trajet composé" << endl;
 		cout << "3. Recherche" << endl;
@@ -14,6 +16,8 @@ int main() {
 		cout << "5. Recherche avancée" << endl;
 
 		cin >> temp;
+
+		if()
 	}
 
 	return 0;
