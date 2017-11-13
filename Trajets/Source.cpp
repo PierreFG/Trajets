59c09e80aba64bb0564c32563ce757b43cab908b
Pierre Faure--Giovagnoli <pierre.faure.giovagnoli@gmail.com>
2017-11-13 15:49:53 +0100

    Trajet

---
+++ Trajets/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	cout << "/////////////// POLYMORPHISME ///////////////";
+
+	string temp;
+	while (1) {
+		cout << "1. Ajouter trajet simple" << endl;
+		cout << "2. Ajouter trajet composé" << endl;
+		cout << "3. Recherche" << endl;
+		cout << "4. Recherche avancée" << endl;
+		cout << "5. Recherche avancée" << endl;
+
+		cin >> temp;
+	}
+
+	return 0;
+}
